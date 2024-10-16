4ba47210d4052f12ac17c9f7158e7ad04f23c534
Rami Maalouf <rami.rami@ucalgary.ca>
2024-10-16 09:52:41 -0600

    initialized barebones app

---
+++ Orbit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Orbit
+//
+//  Created by Rami Maalouf on 2024-10-15.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @State var items: [Item] = []
+
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
